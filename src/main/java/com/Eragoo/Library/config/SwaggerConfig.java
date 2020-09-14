01368d905c25b576fca38b39d92eec58e713d63c
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-09-14 08:56:12 +0300

    unused imports removed

--- src/main/java/com/Eragoo/Library/config/SwaggerConfig.java
+++ src/main/java/com/Eragoo/Library/config/SwaggerConfig.java
@@ -1,15 +1,12 @@
 package com.Eragoo.Library.config;
 
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.spring.web.plugins.Docket;
-
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @Configuration
-@AllArgsConstructor
 public class SwaggerConfig {
     @Bean
     Docket swaggerDocket() {
