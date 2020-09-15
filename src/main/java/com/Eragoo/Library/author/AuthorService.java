633910748e867aaf74739946bd1facad940e0a6b
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-09-15 19:39:27 +0300

    in AuthorService, GenreService validation added

--- src/main/java/com/Eragoo/Library/author/AuthorService.java
+++ src/main/java/com/Eragoo/Library/author/AuthorService.java
@@ -1,7 +1,9 @@
 package com.Eragoo.Library.author;
 
+import com.Eragoo.Library.error.exception.ConflictException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,10 +14,20 @@ public class AuthorService {
     private AuthorRepository authorRepository;
     private AuthorMapper authorMapper;
 
+    @Transactional
     public AuthorDto create(AuthorCommand authorCommand) {
+        checkExistingAuthorByName(authorCommand.getName());
+
         Author author = authorMapper.commandToEntity(authorCommand);
         Author saved = authorRepository.save(author);
         return authorMapper.entityToDto(saved);
+    }
+
+    private void checkExistingAuthorByName(String name) {
+        authorRepository.findByName(name)
+                .ifPresent((author) -> {
+                    throw new ConflictException("Author with that name already exist");
+                });
     }
 
     public List<AuthorDto> getAll() {
--- src/main/java/com/Eragoo/Library/genre/GenreService.java
+++ src/main/java/com/Eragoo/Library/genre/GenreService.java
@@ -1,5 +1,6 @@
 package com.Eragoo.Library.genre;
 
+import com.Eragoo.Library.error.exception.ConflictException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,17 @@ public class GenreService {
     private GenreMapper genreMapper;
 
     public GenreDto create(GenreCommand command) {
+        checkExistingGenreByName(command.getName());
         Genre genre = genreMapper.commandToEntity(command);
         Genre savedGenre = genreRepository.save(genre);
         return genreMapper.entityToDto(savedGenre);
+    }
+
+    private void checkExistingGenreByName(String name) {
+        genreRepository.findByName(name)
+                .ifPresent((genre) -> {
+                    throw new ConflictException("Genre with that name already exist");
+                });
     }
 
     public List<GenreDto> getAll() {
